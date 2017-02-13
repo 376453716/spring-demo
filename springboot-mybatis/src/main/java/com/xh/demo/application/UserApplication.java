7ad312e0cf58d93a457391bce51c8f9147c634d7
xiongh <376453716@qq.com>
2017-02-13 22:41:20 +0800

    spring boot mybatis init

---
+++ springboot-mybatis/src/main/java/com/xh/demo/application/UserApplication.java
@@ -0,0 +1,15 @@
+package com.xh.demo.application;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+
+/**
+ * @author Xiong Hao
+ */
+@EnableAutoConfiguration
+public class UserApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UserApplication.class);
+    }
+}
---
+++ springboot-mybatis/src/main/java/com/xh/demo/entity/UserInfo.java
@@ -0,0 +1,46 @@
+package com.xh.demo.entity;
+
+import java.util.Date;
+
+/**
+ * @author Xiong Hao
+ */
+public class UserInfo {
+
+    private long id;
+    private String name;
+    private Date birthday;
+    private String email;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Date getBirthday() {
+        return birthday;
+    }
+
+    public void setBirthday(Date birthday) {
+        this.birthday = birthday;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
